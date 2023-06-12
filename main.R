06cee76d827b6812fe88c2613d13f8ce128f147c
TimonNetzel <134919762+TimonNetzel@users.noreply.github.com>
2023-06-12 07:56:58 +0200

    Add files via upload

--- main.R
+++ main.R
@@ -17,7 +17,7 @@ thin_size <- 75
 
 # load functions 
 sourceCpp(file='src/Rcpp_functions.cpp')
-source("src/my_functions.R")
+source("src/R_functions.R")
 
 # load data and prepare the MCMC sampling
 source("src/load_data.R")
@@ -30,5 +30,3 @@ if(mcmc_sampling){
 
 # post processing of the MCMC samples and plotting
 source("src/mcmc_postprocessing.R")
-
-
